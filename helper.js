82d8870223c1661ea67222ba3ee36f07355474db
Nils Kenneweg <software@ovt.me>
2014-04-01 17:40:44 +0200

    add datauritoblob helper.

--- helper.js
+++ helper.js
@@ -5,6 +5,28 @@ var helper = {
 	/** to disable logging (console.log) which is necessary because logger.js depends on helper */
 	log: true,
 
+	dataURItoBlob: function (dataURI) {
+		if (atob && Blob && ArrayBuffer && Uint8Array) {
+			// convert base64 to raw binary data held in a string
+			// doesn't handle URLEncoded DataURIs
+			var byteString = atob(dataURI.split(",")[1]);
+
+			// separate out the mime component
+			var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+			// write the bytes of the string to an ArrayBuffer
+			var ab = new ArrayBuffer(byteString.length);
+			var ia = new Uint8Array(ab);
+			for (var i = 0; i < byteString.length; i++) {
+				ia[i] = byteString.charCodeAt(i);
+			}
+
+			return new Blob([ab], { type: mimeString });
+		} else {
+			return false;
+		}
+	},
+
 	/** calls all listeners */
 	callListener: function(listener, arg1) {
 		var i;
