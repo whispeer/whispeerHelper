033620dd910500be75a21b1a3c46e69ffa5a6b84
Nils Kenneweg <nk@ovt.me>
2017-07-19 10:14:02 +0200

    remove unused

--- helper.ts
+++ helper.ts
@@ -1,10 +1,8 @@
 "use strict";
 
 declare var webkitURL: any
-declare var define: any
 declare var global: any
 declare var process: any
-declare var module: any
 
 interface MyWindow extends Window {
 	BlobBuilder, WebKitBlobBuilder, MozBlobBuilder, MSBlobBuilder
